de1576a85424e6713d2332671b292696474bc1d8
Sushant <sush1393@gmail.com>
2021-03-17 15:30:32 +0530

    Added comments and corrected mistakes

--- Arrays/reverse.js
+++ Arrays/reverse.js
@@ -1,44 +1,46 @@
-// Reverse a given input - string or number
-
-// Brute force method
-function reverse(input) {
+let reverse = (input) => {
     if (typeof input === 'string') {
-        let result = '';
+        let reversedStr = '';
         for (let i = input.length - 1; i >= 0; i--) {
-            result = result + input[i];
+            reversedStr += input[i];
         }
-        return result;
+        return reversedStr;
     } else if (typeof input === 'number') {
-        let digit, result = 0;
+        let reversedNum, digit = 0;
         while (input) {
-            digit = input % 10;
-            result = (result * 10) + digit;
-            input = input / 10 | 0;
+            digit = input % 10; // This will give us the last digit
+            reversedNum = reversedNum * 10 + digit; // For eg. if the number is 1234, then in the second iteration the digit would be 3 and reversedNum = 40 + 3 = 43
+            input = input / 10 | 0; // Will remove the decimal point
         }
-        return result;
+        return reversedNum;
     } else {
         return "Please enter a valid input";
     }
 }
 
-// Reversing an input using ES6
+reverse('Hello');
+
+// Output = olleh
+
+reverse(12345);
+
+// Output - 54321
+
+// Using built-in methods
 let reverse2 = (input) => {
     if (typeof input === 'string') {
-        return [...input].reverse().join();
+        return [...input].reverse().join('');
     } else if (typeof input === 'number') {
         return parseInt(input.toString().split('').reverse().join(''));
     } else {
-        return 'Please enter a valid input';
+        return "Please enter a valid input";
     }
 }
 
-reverse('hi there');
-// Output - 'ereht ih'
-reverse(1234);
-// Output - 4321
+reverse('Hello');
 
-reverse2('hi there');
-// Output - 'ereht ih'
-reverse2(1234);
-// Output - 4321
+// Output = olleh
 
+reverse(12345);
+
+// Output - 54321
